542af7c725906dc36db531d195d2278c96f0b300
thelando618 <landonzirkelbach@gmail.com>
2012-09-11 19:56:11 -0500

    Finalizing footer.

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		<div class="twelvecol">
 			
-			<h1>AVAILABLE FOR FREELANCE PROJECTS <a href="mailto:landonzirkelbach@gmail.com" class="btn">Contact Me</a></h1>	
+			<h1>AVAILABLE FOR FREELANCE PROJECTS</h1><a href="mailto:landonzirkelbach@gmail.com" class="btn">Contact Me</a>	
 		</div>
 		
 	</div>
