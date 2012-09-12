f8c9974fd5ff1e42eaf1746d6644b3082c12c821
thelando618 <landonzirkelbach@gmail.com>
2012-09-11 22:06:42 -0500

    Staging images. Jquery animation for contact icons. Portfolio building.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-	/*$('.get-in-touch .icons a img').mouseenter(function() {
-    	$(this).effect('bounce',500);
-	});*/
+	$('.get-in-touch .icons img').hover(function() {
+		console.log('this is being hovered');
+		$(this).animate({
+    		top: '-=5',
+  		}, 500, function() {
+    		// Animation complete.
+  		});
+	}, function() {
+  		$(this).animate({
+    		top: '+=5',
+  		}, 250, function() {
+	    	// Animation complete.
+  		});	
+	});
 });
--- portfolio.php
+++ portfolio.php
@@ -20,7 +20,7 @@ include 'includes/header.php';
 	<div class="row">
 		<div class="threecol">
 			<div class="portfolio-item">
-				<img src="images/home-aisle.jpg" />
+				<img src="images/portfolio-aisle.jpg" />
 			</div>
 			<p>Wordpress with multiple custom post types. Marketing site that advertises groceries store app.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
@@ -28,29 +28,46 @@ include 'includes/header.php';
 		</div>
 		<div class="threecol">
 			<div class="portfolio-item">
-				<img src="images/home-aisle.jpg" />
+				<img src="images/portfolio-jli.jpg" />
 			</div>
-			<p>Wordpress with multiple custom post types. Marketing site that advertises groceries store app.</p>
+			<p>Wordpress with multiple custom post types. Reached large amounts of users due to large social media presence.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
-			<p><a href="http://www.aisle411.com" target="_blank">Visit Site</a></p>
+			<p><a href="http://www.justlabelit.org" target="_blank">Visit Site</a></p>
 		</div>
 		<div class="threecol">
 			<div class="portfolio-item">
-				<img src="images/home-aisle.jpg" />
+				<img src="images/portfolio-integrity.jpg" />
 			</div>
-			<p>Wordpress with multiple custom post types. Marketing site that advertises groceries store app.</p>
+			<p>Responsive Wordpress site optimized for any viewing experience.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
-			<p><a href="http://www.aisle411.com" target="_blank">Visit Site</a></p>
+			<p><a href="http://www.responsivegui.com" target="_blank">Visit Site</a></p>
 		</div>
 		<div class="threecol last">
 			<div class="portfolio-item">
-				<img src="images/home-aisle.jpg" />
+				<img src="images/portfolio-nu.jpg" />
 			</div>
-			<p>Wordpress with multiple custom post types. Marketing site that advertises groceries store app.</p>
+			<p>Old Joomla site that was converted to current version of Wordpress. Multiple custom post types and loops were implemented.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
-			<p><a href="http://www.aisle411.com" target="_blank">Visit Site</a></p>
+			<p><a href="http://www.psychiatry.northwestern.edu" target="_blank">Visit Site</a></p>
 		</div>						
-	</div>
+	</div><!-- End Row -->
+	<div class="row">
+		<div class="threecol">
+			<div class="portfolio-item">
+				<img src="images/portfolio-marathon.jpg" />
+			</div>
+			<p>Landing page built for lead conversion. Sends form data through API calls to 3rd part server.</p>
+			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
+			<p><a href="http://www.marathonplanusa.com" target="_blank">Visit Site</a></p>			
+		</div>
+		<div class="threecol">
+			<div class="portfolio-item">
+				<img src="images/portfolio-tara.jpg" />
+			</div>
+			<p>Custom Drupal site. Site has the ability to add audio demos and categorize accordingly.</p>
+			<p><a href="http://www.taratyler.com" target="_blank">Visit Site</a></p>
+		</div>
+	</div><!-- End Row -->
 </div>
 	
 
