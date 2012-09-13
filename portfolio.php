a0df54c8382859dfb9a7cde11a1cdc188d4272af
thelando618 <landonzirkelbach@gmail.com>
2012-09-12 21:14:47 -0500

    Portfolio item descriptions.

--- portfolio.php
+++ portfolio.php
@@ -71,12 +71,14 @@ include 'includes/header.php';
 			<div class="portfolio-item">
 				<img src="images/portfolio-morrissey.jpg" />
 			</div>
+			<p>Custom Wordpress site with many areas made content manageable for the client.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
 		</div>
 		<div class="threecol last">
 			<div class="portfolio-item">
 				<img src="images/portfolio-jocktalk.jpg" />
 			</div>
+			<p>Social Media platform built for athletes to interact with plans. Built with CodeIgniter. I coded a significant portion of the front-end of this project.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
 		</div>
 	</div><!-- End Row -->
@@ -85,24 +87,28 @@ include 'includes/header.php';
 			<div class="portfolio-item">
 				<img src="images/portfolio-steamblade.jpg" />
 			</div>
+			<p>Custom Wordpress site with multiple custom post types. Custom jQuery calculator and multiple photo galleries. Interacts with third party shopping cart.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
 		</div>
 		<div class="threecol">
 			<div class="portfolio-item">
 				<img src="images/portfolio-steambreaker.jpg" />
 			</div>
+			<p>Custom Wordpress site with multiple custom post types. Features multiple photo galleries. Interacts with third party shopping cart.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
 		</div>
 		<div class="threecol">
 			<div class="portfolio-item">
 				<img src="images/portfolio-autoprotect.jpg" />
 			</div>
+			<p>Another cusom landing page built for lead conversion. Sends form data through API calls to 3rd part server.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
 		</div>
 		<div class="threecol last">
 			<div class="portfolio-item">
 				<img src="images/portfolio-flexineb.jpg" />
 			</div>
+			<p>Wordpress site rebranded and made content manageable for client to be able to update more areas of the site.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
 		</div>
 	</div>
