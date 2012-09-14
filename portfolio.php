7bfaaa1847aa97506ee0fb56a2e721dc00b7647c
thelando618 <landonzirkelbach@gmail.com>
2012-09-13 21:34:57 -0500

    Mobile view style fixes. Adding in title and alt tags on portfolio

--- portfolio.php
+++ portfolio.php
@@ -20,7 +20,7 @@ include 'includes/header.php';
 	<div class="row">
 		<div class="threecol">
 			<div class="portfolio-item">
-				<img src="images/portfolio-aisle.jpg" />
+				<img src="images/portfolio-aisle.jpg" alt="Aisle 411 Wordpress Design" title="Aisle 411 Wordpress Design" />
 			</div>
 			<p>Wordpress with multiple custom post types. Marketing site that advertises groceries store app.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
@@ -28,7 +28,7 @@ include 'includes/header.php';
 		</div>
 		<div class="threecol">
 			<div class="portfolio-item">
-				<img src="images/portfolio-jli.jpg" />
+				<img src="images/portfolio-jli.jpg" alt="Just Label It Wordpress Design" title="Just Label It Wordpress Design" />
 			</div>
 			<p>Wordpress with multiple custom post types. Reached large amounts of users due to large social media presence.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
@@ -36,7 +36,7 @@ include 'includes/header.php';
 		</div>
 		<div class="threecol">
 			<div class="portfolio-item">
-				<img src="images/portfolio-integrity.jpg" />
+				<img src="images/portfolio-integrity.jpg" alt="Integrity Responsive Wordpress Design" title="Integrity Responsive Wordpress Design" />
 			</div>
 			<p>Responsive Wordpress site optimized for any viewing experience.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
@@ -44,7 +44,7 @@ include 'includes/header.php';
 		</div>
 		<div class="threecol last">
 			<div class="portfolio-item">
-				<img src="images/portfolio-nu.jpg" />
+				<img src="images/portfolio-nu.jpg" alt="Northwestern University Wordpress Design" title="Northwestern University Wordpress Design" />
 			</div>
 			<p>Old Joomla site that was converted to current version of Wordpress. Multiple custom post types and loops were implemented.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
@@ -54,7 +54,7 @@ include 'includes/header.php';
 	<div class="row">
 		<div class="threecol">
 			<div class="portfolio-item">
-				<img src="images/portfolio-marathon.jpg" />
+				<img src="images/portfolio-marathon.jpg" alt="Marathon Plan Landing Page" title="Marathon Plan Landing Page" />
 			</div>
 			<p>Landing page built for lead conversion. Sends form data through API calls to 3rd part server.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
@@ -62,21 +62,22 @@ include 'includes/header.php';
 		</div>
 		<div class="threecol">
 			<div class="portfolio-item">
-				<img src="images/portfolio-tara.jpg" />
+				<img src="images/portfolio-tara.jpg" alt="Tara Tyler Drupal site" title="Tara Tyler Drupal site" />
 			</div>
 			<p>Custom Drupal site. Site has the ability to add audio demos and categorize accordingly.</p>
 			<p><a href="http://www.taratyler.com" target="_blank">Visit Site</a></p>
 		</div>
 		<div class="threecol">
 			<div class="portfolio-item">
-				<img src="images/portfolio-morrissey.jpg" />
+				<img src="images/portfolio-morrissey.jpg" alt="Morrissey Construction Wordpress Design" title="Morrissey Construction Wordpress Design" />
 			</div>
 			<p>Custom Wordpress site with many areas made content manageable for the client.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
+			<p><a href="http://www.morrisseyconstruction.com" target="_blank">Visit Site</a></p>
 		</div>
 		<div class="threecol last">
 			<div class="portfolio-item">
-				<img src="images/portfolio-jocktalk.jpg" />
+				<img src="images/portfolio-jocktalk.jpg" alt="JockTakl Social Media Platform" title="JockTalk Social Media Platform" />
 			</div>
 			<p>Social Media platform built for athletes to interact with plans. Built with CodeIgniter. I coded a significant portion of the front-end of this project.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
@@ -86,7 +87,7 @@ include 'includes/header.php';
 	<div class="row">
 		<div class="threecol">
 			<div class="portfolio-item">
-				<img src="images/portfolio-steamblade.jpg" />
+				<img src="images/portfolio-steamblade.jpg" alt="Steamblade Wordpress Design" title="Steamblade Wordpress Design" />
 			</div>
 			<p>Custom Wordpress site with multiple custom post types. Custom jQuery calculator and multiple photo galleries. Interacts with third party shopping cart.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
@@ -94,7 +95,7 @@ include 'includes/header.php';
 		</div>
 		<div class="threecol">
 			<div class="portfolio-item">
-				<img src="images/portfolio-steambreaker.jpg" />
+				<img src="images/portfolio-steambreaker.jpg" alt="Steambreaker Wordpress Design" title="Steambreaker Wordpress Design" />
 			</div>
 			<p>Custom Wordpress site with multiple custom post types. Features multiple photo galleries. Interacts with third party shopping cart.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
@@ -102,7 +103,7 @@ include 'includes/header.php';
 		</div>
 		<div class="threecol">
 			<div class="portfolio-item">
-				<img src="images/portfolio-autoprotect.jpg" />
+				<img src="images/portfolio-autoprotect.jpg" alt="Auto Protect Plan landing page" title="Auto Protect Plan landing page" />
 			</div>
 			<p>Another cusom landing page built for lead conversion. Sends form data through API calls to 3rd part server.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
@@ -110,7 +111,7 @@ include 'includes/header.php';
 		</div>
 		<div class="threecol last">
 			<div class="portfolio-item">
-				<img src="images/portfolio-flexineb.jpg" />
+				<img src="images/portfolio-flexineb.jpg" alt="Flexineb Wordpress Design" title="Flexineb Wordpress Design" />
 			</div>
 			<p>Wordpress site rebranded and made content manageable for client to be able to update more areas of the site.</p>
 			<p>Completed while at <a href="http://integritystl.com/" target="_blank">Integrity</a>.</p>
